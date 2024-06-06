921217a5ae781f6f505283b0b568e73144170f45
Ridhya <ridhyapunj@gmail.com>
2024-06-06 15:58:12 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+let name="Rohan"
+let age=19
+let isLoggedIn=false
+let state=null;
+
+
+// primitive datatypes
+//number => 2^53
+// bigint => very large number
+// string => ""
+//boolean => true/false
+//null => standalone value / a representation of empty value.
+//undefined =>nothing but not equal to null
+//symbol => unique
+
+
+//non-primitive
+//object
+
+
+console.log(typeof name)  //string
+console.log(typeof null)  //object
+console.log(typeof undefined)  //undefined
