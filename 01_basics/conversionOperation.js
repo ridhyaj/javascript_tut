f96480b5655117c61d706edb567682d477db4916
Ridhya <ridhyapunj@gmail.com>
2024-06-06 16:36:13 +0000

    datatype conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,39 @@
+let score="33"
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+let s="33abbs"
+let valueNumber=Number(s)
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+
+// "33" => 33
+// "33abbs" => NaN
+// "True" => 1
+// null => 0
+// undefined => undefined
+
+
+
+
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+//1=true
+//0=false
+//""=false
+//"Rohan"=true
+
+let num=22
+let stNum=String(num)
+console.log(typeof stNum);
