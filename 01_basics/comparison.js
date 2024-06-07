6fd8c5f604aa669918f7acaf717390ed10b2a7bb
Ridhya <ridhyapunj@gmail.com>
2024-06-07 17:54:12 +0000

    comparison of different datatypes

---
+++ 01_basics/comparison.js
@@ -0,0 +1,15 @@
+//simple comaprisons
+console.log(2>1);
+console.log(2<=1);
+console.log(2!=1);
+
+//comapring different datatypes
+console.log("2">1);  //true
+console.log("02">1)  //true
+
+console.log(null>0)  //false
+console.log(null==0);  //false
+console.log(null>=0);  //true
+
+//the equality check (==) and comaprison (<,>,>=,<=) works differently. The comparison convert null to a number, treating it as 0, that's why it gave true.
+//same as undefined
