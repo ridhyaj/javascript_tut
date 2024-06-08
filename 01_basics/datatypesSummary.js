a5d3c62ed3e344c7fba0b5ffb9b5d268c19a70ec
Ridhya <ridhyapunj@gmail.com>
2024-06-08 07:36:48 +0000

    primitive datatype

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,28 @@
+//Primitive Datatype
+//7types: String, Number, boolean, null, undefined, Symbol, BigInt
+
+//number
+const score=100
+const scoreValue=100.3
+
+//boolean
+const isLoggedIn=false
+
+//null
+const outsideTemp=null
+
+//undefined
+let user=undefined
+let userEmail;
+
+//symbol
+const id1=Symbol('123')
+const id2=Symbol('123')
+
+console.log(id1===id2);
+
+//bigInt
+const bigNumber=34567876543234567654n
+
+//Reference type / Non-primitive
+//Array, Objects, Functions, 
